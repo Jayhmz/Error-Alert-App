9f9a501ab76f8ef36b2bf7caf46f5875f70b7dc6
Jayhmz <Jayhmz10@gmail.com>
2024-02-27 18:47:56 +0100

    feat: ico count of alerts

---
+++ src/main/java/com/plantaccion/alartapp/ico/controller/CountAlertByICORegionController.java
@@ -0,0 +1,28 @@
+package com.plantaccion.alartapp.ico.controller;
+
+import com.plantaccion.alartapp.ico.service.CountAlertByClusterService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app/v1/ico")
+public class CountAlertByICORegionController {
+    private final CountAlertByClusterService countAlertByClusterService;
+
+    public CountAlertByICORegionController(CountAlertByClusterService countAlertByClusterService) {
+        this.countAlertByClusterService = countAlertByClusterService;
+    }
+
+    @GetMapping("/count-alerts-in-zone")
+    private ResponseEntity<?> countAlertByIcoCluster(){
+        return new ResponseEntity<>(countAlertByClusterService.countAlertByCluster(), HttpStatus.OK);
+    }
+
+    @GetMapping("/count-alerts-resolved-by-ico")
+    private ResponseEntity<?> countAlertResolvedByIco(){
+        return new ResponseEntity<>(countAlertByClusterService.countAlertReviewedByICO(), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/plantaccion/alartapp/ico/service/CountAlertByClusterServiceImpl.java
@@ -0,0 +1,33 @@
+package com.plantaccion.alartapp.ico.service;
+
+import com.plantaccion.alartapp.common.repository.AlertRepository;
+import com.plantaccion.alartapp.common.repository.ICOProfileRepository;
+import com.plantaccion.alartapp.common.utils.AppUtils;
+import com.plantaccion.alartapp.exception.StaffNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CountAlertByClusterServiceImpl implements CountAlertByClusterService{
+    private final AlertRepository alertRepository;
+    private final ICOProfileRepository icoProfileRepository;
+
+    public CountAlertByClusterServiceImpl(AlertRepository alertRepository, ICOProfileRepository icoProfileRepository) {
+        this.alertRepository = alertRepository;
+        this.icoProfileRepository = icoProfileRepository;
+    }
+
+    @Override
+    public int countAlertByCluster() {
+        var user = AppUtils.getAuthenticatedUserDetails()
+                .orElseThrow(() -> new StaffNotFoundException("Unknown Staff/User"));
+        var ico = icoProfileRepository.findByStaffId(user.getStaffId());
+        return alertRepository.countAlertByCluster(ico.getOnboardedBy().getCluster());
+    }
+    @Override
+    public int countAlertReviewedByICO() {
+        var user = AppUtils.getAuthenticatedUserDetails()
+                .orElseThrow(() -> new StaffNotFoundException("Unknown Staff/User"));
+        var ico = icoProfileRepository.findByStaffId(user.getStaffId());
+        return alertRepository.countAlertReviewedByICO(ico.getOnboardedBy().getCluster(), ico.getIcoStaff());
+    }
+}
